aea06a31b46457cd031bfa0960b32fde4b001fd9
Fabricio Bedin <fabricioobedin@gmail.com>
2020-03-04 00:02:05 -0300

    adding mongodb and notifications

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import AppointmentController from './app/controllers/AppointmentController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -24,6 +25,8 @@ routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 routes.get('/providers', ProviderController.index);
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 routes.get('/schedule', ScheduleController.index);
 
 export default routes;
