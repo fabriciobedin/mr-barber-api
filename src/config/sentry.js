ef4d5f764c163408a049180f9b23b1d7990f37c4
Fabricio Bedin <fabricioobedin@gmail.com>
2020-03-22 21:59:25 -0300

    starting with automated tests

--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,5 +1,5 @@
 export default {
-  dsn: process.env.SENTRY_DSN
+  dsn:
+    process.env.SENTRY_DSN ||
+    'https://68aea14a898b45fe8d061b38ff9ae519@sentry.io/5170780'
 };
-
-('https://68aea14a898b45fe8d061b38ff9ae519@sentry.io/5170780');
